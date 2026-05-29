3cc864fee366babf968362c943641c80ee8c7aa0
aaacarpet631-design <aaacarpet631@gmail.com>
2026-05-29 18:53:52 -0500

    Add files via upload

---
+++ customer-store.js
@@ -0,0 +1,10 @@
+// customer-store.js placeholder for AAA HyperKernel
+;(function(global){
+  'use strict';
+  const store = {
+    async boot() {
+      // no-op
+    }
+  };
+  global.AAA_CUSTOMER_STORE = store;
+})(typeof window !== 'undefined' ? window : this);
