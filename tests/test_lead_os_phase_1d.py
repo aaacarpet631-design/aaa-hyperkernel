301311755a856241dd2801365d687dbfab619cc5
aaacarpet631-design <aaacarpet631@gmail.com>
2026-07-09 10:13:13 -0500

    Harden Lead OS tests for package import path

--- tests/test_lead_os_phase_1d.py
+++ tests/test_lead_os_phase_1d.py
@@ -1,3 +1,10 @@
+import pathlib
+import sys
+
+ROOT = pathlib.Path(__file__).resolve().parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 from lead_os.agent_auth import AgentCredentialStore, AgentIdentity, ScopeDeniedError
 from lead_os.outcomes import OutcomeRegistry, OutcomeType, build_scorecards
 from lead_os.supervisor import ApprovalStatus, SupervisorLedger, requires_approval
