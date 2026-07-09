0b254941ddd747880684c1fab1c3110c94f7b0a9
aaacarpet631-design <aaacarpet631@gmail.com>
2026-07-09 10:04:08 -0500

    Export Lead OS Phase 1D core APIs

--- lead_os/__init__.py
+++ lead_os/__init__.py
@@ -4,13 +4,25 @@ This package is intentionally framework-light so it can be mounted into the
 Hermes/FastAPI Lead OS service without forcing a specific web stack.
 """
 
-from .agent_auth import AgentCredentialStore, AgentIdentity, ScopeDeniedError
-from .supervisor import AgentAction, SupervisorLedger
+from .agent_auth import (
+    AgentCredentialStore,
+    AgentIdentity,
+    ScopeDeniedError,
+    default_agent_identities,
+)
+from .outcomes import OutcomeRegistry, OutcomeType, build_scorecards
+from .supervisor import AgentAction, ApprovalStatus, SupervisorLedger, requires_approval
 
 __all__ = [
     "AgentAction",
     "AgentCredentialStore",
     "AgentIdentity",
+    "ApprovalStatus",
+    "OutcomeRegistry",
+    "OutcomeType",
     "ScopeDeniedError",
     "SupervisorLedger",
+    "build_scorecards",
+    "default_agent_identities",
+    "requires_approval",
 ]
